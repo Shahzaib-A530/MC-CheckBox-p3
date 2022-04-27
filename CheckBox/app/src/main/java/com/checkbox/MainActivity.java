3059476ea8e8370c5b4b1b15c1dbd0b83439d9f4
Shahzaib-A530 <bsef19a530@pucit.edu.pk>
2022-04-27 16:23:23 +0500

    First commit

---
+++ CheckBox/app/src/main/java/com/checkbox/MainActivity.java
@@ -0,0 +1,72 @@
+package com.checkbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    CheckBox checkBoxDone, checkBoxPending, checkBoxRead;
+
+    Button simpleButton2;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        checkBoxDone = findViewById(R.id.checkBoxDone);
+
+        checkBoxDone.setOnClickListener(this);
+
+        checkBoxPending = findViewById(R.id.checkBoxPadding);
+
+        checkBoxPending.setOnClickListener(this);
+
+        checkBoxRead = findViewById(R.id.checkBoxRead);
+
+        checkBoxRead.setOnClickListener(this);
+
+        simpleButton2 = findViewById(R.id.simpleButton2);
+
+    }
+
+    @Override
+
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.checkBoxDone:
+
+                if (checkBoxDone.isChecked())
+
+                    simpleButton2.setText("Check Box Done");
+
+                break;
+
+            case R.id.checkBoxPadding:
+
+                if (checkBoxPending.isChecked())
+
+                    simpleButton2.setText("Check Box Pending");
+
+                break;
+
+            case R.id.checkBoxRead:
+
+                if (checkBoxRead.isChecked())
+
+                    simpleButton2.setText("Check Box Read");
+
+                break;
+
+        }
+
+    }
+}
